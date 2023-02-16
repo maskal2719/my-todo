9d83023153eae7d332f4ae45e56ad15c3560137a
Stanislav Moskalenko <maskal2719@gmail.com>
2023-02-16 16:32:59 +0300

    refactor: add Destructuring props in some components

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,17 +12,17 @@ type TodoListPropsType = {
     addTask: (title: string) => void;
 }
 
-const TodoList = (props: TodoListPropsType) => {
+const TodoList:React.FC<TodoListPropsType> = ({title, tasks,changeFilter,removeTask, addTask}) => {
 
-    const setFilterAll = () => props.changeFilter('all')
-    const setFilterActive = () =>  props.changeFilter('active')
-    const setFilterCompleted = () => props.changeFilter('completed')
+    const setFilterAll = () => changeFilter('all')
+    const setFilterActive = () =>  changeFilter('active')
+    const setFilterCompleted = () => changeFilter('completed')
 
     return (
         <div>
-            <h1>{props.title}</h1>
-            <Input addNewTask={props.addTask}/>
-            <Tasks tasks={props.tasks} removeTask={props.removeTask}/>
+            <h1>{title}</h1>
+            <Input addNewTask={addTask}/>
+            <Tasks tasks={tasks} removeTask={removeTask}/>
             <Button title={'All'} callBack={setFilterAll}/>
             <Button title={'Active'} callBack={setFilterActive}/>
             <Button title={'Completed'} callBack={setFilterCompleted}/>
